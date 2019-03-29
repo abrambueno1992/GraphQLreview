d67310662c49602572cfe39d094119dbea78b69f
Abraham Bueno <abueno@localhost.localdomain>
2019-03-29 00:29:15 -0500

    Added missing closing bracket and made the info data return a String

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,39 +1,39 @@
 const { GraphQLServer} = require("graphql-yoga")
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
 
-type Link {
-id: ID!
-description: String!
-url: String!
-}
-`
 
-let links = [{
+const links = [{
     id: "link-0",
     url: "www.howtographql.com",
     description: "Fullstack tutorial for GraphQL"
 }]
-
+let idCount = links.length
 const resolvers = {
     Query: {
-        info: () => null, // "This is the API of a Hackernews Clone",
+        info: () => "This is the API of a Hackernews Clone",
         feed: () => links,
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
     },
     Link: {
         id: (parent) => parent.id,
         description: (parent) => parent.description,
         url: (parent) => parent.url,
-    }
+    },
 }
 
-const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
-})
 
+const server = new GraphQLServer({
+    typeDefs: "./src/schema.graphql",
+    resolvers
+})
 server.start(() => console.log("Server is running on http://localhost:4000"))
