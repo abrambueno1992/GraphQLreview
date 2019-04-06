63895dff56bb085e32c6c86c1cce782ebde14bab
Abraham Bueno <abueno@localhost.localdomain>
2019-04-05 23:05:26 -0500

    Changes

--- hackernews-node/src/resolvers/Query.js
+++ hackernews-node/src/resolvers/Query.js
@@ -3,30 +3,29 @@ async function feed(parent, args, context) {
     ? {
         OR: [
           { description_contains: args.filter },
-          { url_contains: args.filter },
-        ],
+          { url_contains: args.filter }
+        ]
       }
-    : {}
+    : {};
 
   const links = await context.prisma.links({
     where,
     skip: args.skip,
     first: args.first,
-    orderBy: args.orderBy,
-  })
+    orderBy: args.orderBy
+  });
   const count = await context.prisma
     .linksConnection({
-      where,
-      skip: args.skip,
+      where
     })
     .aggregate()
-    .count()
+    .count();
   return {
     links,
-    count,
-  }
+    count
+  };
 }
 
 module.exports = {
-  feed,
-}
+  feed
+};
